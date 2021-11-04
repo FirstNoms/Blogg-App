96823057f5cb444b5552fc2183780054b7a688e7
Confidence Okere <okerenomso@gmail.com>
2021-11-04 15:45:18 +0100

    Test To Find Delete a Comment.

--- src/test/java/com/blogApp/blog/data/repository/CommentRepositoryTest.java
+++ src/test/java/com/blogApp/blog/data/repository/CommentRepositoryTest.java
@@ -35,7 +35,26 @@ class CommentRepositoryTest {
     @Test
     void testToFindCommentById(){
         Comment comment1  = commentRepository.findById(10l).orElse(null);
+        Comment comment2 = commentRepository.findById(11l).orElse(null);
+        Comment comment3 = commentRepository.findById(12l).orElse(null);
         assertThat(comment1.getId()).isNotNull();
+        assertThat(comment2.getId()).isNotNull();
+        assertThat(comment3.getId()).isNotNull();
         assertThat(comment1.getName()).isEqualTo("Chinomso");
+        assertThat(comment2.getBody()).isEqualTo("I am a Software Developer");
+        assertThat(comment3.getBody()).isEqualTo("I am a Front-end developer and designer");
+
+    }
+
+    @Test
+    void testToDeleteComment(){
+        //when
+        Comment comment = commentRepository.findById(12l).orElse(null);
+        //And
+        assertThat(comment).isNotNull();
+        commentRepository.delete(comment);
+        //then
+        Comment comment2 = commentRepository.findById(12l).orElse(null);
+        assertThat(comment2).isNull();
     }
 }

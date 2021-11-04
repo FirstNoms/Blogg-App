fc4a1cd1cab8207b344f0a022f5af6bf774c8938
Confidence Okere <okerenomso@gmail.com>
2021-11-04 14:16:25 +0100

    Test To Find Comment By Id. Inserted the values to my database.

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,3 +1,10 @@
 set foreign_key_checks = 0;
 
 truncate table comment;
+insert into comment(`id`,`body`, `name`)
+values(10, 'I am a Technical Product Manager', 'Chinomso'),
+      (11, 'I am a Software Developer','Janet'),
+      (12, 'I am a Front-end developer and designer', 'Blessing');
+
+set foreign_key_checks = 1;
+
--- src/test/java/com/blogApp/blog/data/repository/CommentRepositoryTest.java
+++ src/test/java/com/blogApp/blog/data/repository/CommentRepositoryTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
+@Sql(scripts={"/db/insert.sql"})    //link up your comment database values with the comment test.
 class CommentRepositoryTest {
     @Autowired
     CommentRepository commentRepository;
@@ -28,5 +30,13 @@ class CommentRepositoryTest {
         comment.setName("This indicates the Name");
         commentRepository.save(comment);
         assertThat(comment.getId()).isNotNull();
+    }
+
+    @Test
+    void testToFindCommentById(){
+        Comment comment1  = commentRepository.findById(10l).orElse(null);
+        assertThat(comment1.getId()).isNotNull();
+
+
     }
 }
